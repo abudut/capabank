784e298234114e6532f1ba3a9ce114c874f2cb55
Abudu <abudutouray1999.at@gmail.com>
2020-05-07 23:13:22 +0200

    first commit

--- application/views/pages/users.php
+++ application/views/pages/users.php
@@ -10,6 +10,7 @@
 					<th>Grupo</th>
 					<th>Estado</th>
 					<th>Accion</th>
+				
 				</tr>
 				<?php
 				foreach ($users as $user) {
@@ -22,13 +23,13 @@
 								<td>    </td>
 								<td> ";
 					if ($user->getActive() == 1) {
-						echo '<button class="btn btn-success user_status" uid="' . $user->getId() . '"  ustatus="'.$user->getActive().'">Activo</button>';
+						echo '<button class="btn btn-success user_status" uid="' . $user->getId() . '"  ustatus="' . $user->getActive() . '">Activo</button>';
 					} else {
-						echo '<button class="btn btn-danger user_status" uid="' . $user->getId() . '"  ustatus="'.$user->getActive().'">Inactivo</button>';
+						echo '<button class="btn btn-danger user_status" uid="' . $user->getId() . '"  ustatus="' . $user->getActive() . '">Inactivo</button>';
 					}
 					"</td>
-					<td><button class='btn btn-warning'>Editar</button></td>
-			
+							<td><button class='btn btn-warning'>Editar</button></td>
+					
 							</tr>";
 				}
 				?>
@@ -39,17 +40,17 @@
 			<script type="text/javascript">
 				$(document).on('click', '.user_status', function() {
 
-					var id = $(this).attr('uid'); 
-					var status = $(this).attr('ustatus'); 
+					var id = $(this).attr('uid');
+					var status = $(this).attr('ustatus');
 
-					$('#user_id').val(id); 
-					$('#user_status').val(status); 
+					$('#user_id').val(id);
+					$('#user_status').val(status);
 
 					$('#modal_popup').modal({
 						backdrop: 'static',
 						keyboard: true,
 						show: true
-					}); 
+					});
 
 				});
 			</script>
@@ -58,7 +59,7 @@
 
 				<div class="modal-dialog modal-sm">
 
-				
+
 					<form action="<?php echo base_url(); ?>users/updateStatus" method="post">
 						<div class="modal-content">
 
@@ -66,7 +67,7 @@
 
 								<h4 style="margin-top: 50px;text-align: center;">Seguro que quieres canviar el estado del usuario?</h4>
 
-								
+
 								<input type="hidden" name="id" id="user_id" value="">
 								<input type="hidden" name="status" id="user_status" value="">
 
@@ -88,14 +89,14 @@
 
 			</div>
 
-			<?php if($error = $this->session->flashdata('msg')){ ?>
+			<?php if ($error = $this->session->flashdata('msg')) { ?>
 
-<h3 class="text-success"><?php echo  $error; ?></h3>
+				<h3 class="text-success"><?php echo  $error; ?></h3>
 
-<?php } ?>
+			<?php } ?>
 
 
-			<a class="w3-button" href="<?php echo base_url('users/add'); ?>">Afegir un nou usuari</a>
+			<a class="btn btn-primary" href="<?php echo base_url('users/add'); ?>">Afegir un nou usuari</a>
 
 
 		</div>
